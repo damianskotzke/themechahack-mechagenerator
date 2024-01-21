d49d55409b0ce931a28ca94180b91b9fd918db9e
Damian Skotzke <damianskotzke@Damians-MacBook-Pro.local>
2024-01-21 14:57:08 +0100

    fixing fonts according to next.js documentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({
+  src: [
+    {
+      path: "/JetBrainsMono-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./JetBrainsMono-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "The Mecha Hack | Mecha Generator",
@@ -16,7 +29,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-stone-950 grid gap-2 p-2">{children}</body>
+      <body className={`${myFont.className} bg-stone-950 grid gap-2 p-2`}>
+        {children}
+      </body>
     </html>
   );
 }
